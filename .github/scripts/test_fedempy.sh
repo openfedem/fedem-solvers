ec4054c36413d1c0032ce660276f3b6eebf5adc4
Knut Morten Okstad <knut.morten.okstad@sintef.no>
2025-12-23 16:27:04 +0100

    Fixup for commit 833e490af26ecc0ea9b9d7847bfd4bc6483608de

--- .github/scripts/test_fedempy.sh
+++ .github/scripts/test_fedempy.sh
@@ -7,17 +7,22 @@
 
 # Runs all (or most of) the fedempy regression tests.
 
-if [ $# -lt 1 ]; then exec echo "usage: $0 <test-directory>"; fi
-if [ ! -d $1 ]; then exec echo "$0: No such directory: $1"; fi
+set -e
 
-if [ -d PythonAPI/PythonAPITests ]; then
-  cd PythonAPI/PythonAPITests
+if [ $# -lt 1 ]; then echo "usage: $0 <test-directory>"; exit 1; fi
+if [ ! -d $1 ]; then echo "$0: No such directory: $1"; exit 1; fi
+
+FEDEMPY_ROOT=$PWD/PythonAPI
+if [ -d $FEDEMPY_ROOT/PythonAPITests ]; then
+  export PYTHONPATH=$FEDEMPY_ROOT/src:$FEDEMPY_ROOT/PythonAPITests
+  cd $FEDEMPY_ROOT/PythonAPITests
   TEST_DIR=$1/TimeDomain python3 -B test_api.py
   TEST_DIR=$1/TimeDomain python3 -B test_extfunc.py
   TEST_DIR=$1/TimeDomain python3 -B test_Microbatch.py
 fi
 
-for file in `find $1 -name run_API.py`; do
+export PYTHONPATH=$FEDEMPY_ROOT/src:$1/InversePy
+for file in `find $1 -name run_API.py | sort`; do
   cd `dirname $file`; pwd
   python3 -B run_API.py --no-print
 done
